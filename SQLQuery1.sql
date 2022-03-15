f21753f7b9b6f7a5831f732385d15c4255eb5132
Ali Türkmen <84051961+aliturkmen4@users.noreply.github.com>
2022-03-15 18:03:24 +0300

    Add files via upload

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,28 +1,7 @@
----STORED PROCEDURE: Server tarafýndan tutulan bir kez derlendikten sonra bir daha derlenmeyen Sorgudur.
---Parametre içerebilir. 
---Sadece ilk çalýþtýklarýnda derlenir.
---baðýmlýlýklara karþý parametreli yazýlýr böylece sistem güvenliði saðlanýr. 
---kurulum þekli view gibidir. 
-create proc p_KargoFirmaEkle2 as
-insert into Shippers(CompanyName,Phone) values ('Grup165','45778541')
-exec p_KargoFirmaEkle2
-delete from Shippers where ShipperID between 4 and 35
-select*from Shippers
------------------------
-go
-alter proc p_KargoFirmaEkle2
-@cn nvarchar(40),
-@ph nvarchar(24)
-as
-insert Shippers(CompanyName,Phone)values(@cn,@ph)
-exec p_KargoFirmaEkle2 'Grup165-','123456789'
------------------------
---Soru: Category isimlerine göre bir prosedür yazýnýz. yazýnýz prosedürde execte categorisi beverages olanlarý çaðýýrýnýz. 
-go
-create proc CategoryNameList 
-@CN nvarchar(50)
-as
-select * from Products where CategoryID=
-(select CategoryID from Categories where CategoryName=@CN)
-exec CategoryNameList 'Condiments'
+SELECT * FROM CUSTOMERS
 
+SELECT * From CUSTOMERS
+
+UPDATE CUSTOMERS SET AGE=DATEDIFF(YEAR,BIRTHDATE,GETDATE())
+
+DELETE FROM CUSTOMERS  WHERE AGE<38
